464201456c9fc0f9a4807a8159cdc5a8d119ad96
Yevhenns <gifestum@gmail.com>
2023-11-12 12:50:40 +0200

    healthcheck res

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { HealthCheckModule } from './health-check/health-check.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, HealthCheckModule],
   controllers: [AppController],
   providers: [AppService],
 })
