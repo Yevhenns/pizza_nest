78c06108f9abb9a5d77459eb225a455d9e482ce9
Yevhenns <gifestum@gmail.com>
2023-11-12 13:02:03 +0200

    del res

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
-import { HealthCheckModule } from './health-check/health-check.module';
 
 @Module({
-  imports: [ProductsModule, HealthCheckModule],
+  imports: [ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
